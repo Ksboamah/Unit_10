02ed0e9637c4e0e3c378a6e3fbf093009b83b0b3
Kwame <bkwamesakyi@gmail.com>
2019-12-11 08:56:33 -0500

    Initial commit

---
+++ pygame_demo.py
@@ -0,0 +1,18 @@
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+
+main_surface = pygame.display.set_mode((500, 400), 0, 32)
+pygame.display.set_caption("Pygame Demo - So Cool")
+main_surface.fill(255, 255, 255)
+
+pygame.draw.rect(main_surface, (0, 0,255), (50, 50, 200, 100), 0)
+pygame.draw.circle(main_surface, )
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
