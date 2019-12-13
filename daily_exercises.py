9d54a1e4fd614eb5bd2b82a4cb936523eeb6ed1f
Kwame <bkwamesakyi@gmail.com>
2019-12-13 11:21:29 -0500

    I started my Unit 10m daily exercises. I finished my first daily excercise!

--- daily_exercises.py
+++ daily_exercises.py
@@ -7,8 +7,13 @@ main_surface = pygame.display.set_mode((500, 400), 0, 32)
 pygame.display.set_caption("Daily Exercises Creations")
 main_surface.fill((255, 255, 255))
 
-pygame.draw.polygon(main_surface, (102, 255, 255), ((200, 0), (0,200), (100,400), (300, 400), (400, 200)), 0)
-pygame.draw.line(main_surface, (255, 0, 0))
+pygame.draw.polygon(main_surface, (102, 255, 255), ((100, 0), (0, 50), (50, 150), (150, 150), (200, 50)), 0)
+pygame.draw.line(main_surface, (255, 0, 0), (50, 25), (100, 25), 4)
+pygame.draw.line(main_surface, (255, 0, 0), (100, 25), (50, 75), 2)
+pygame.draw.line(main_surface, (255, 0, 0), (50, 75), (100, 75), 4)
+pygame.draw.rect(main_surface, (255, 255, 0), (125, 75, 100, 50), 0)
+pygame.draw.circle(main_surface, (255, 0, 0), (250, 10), 20, 0)
+pygame.draw.ellipse(main_surface, (255, 255, 0), (250, 150, 50, 100), 4)
 
 while True:
     pygame.display.update()
